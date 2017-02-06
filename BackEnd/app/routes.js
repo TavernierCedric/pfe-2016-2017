141b122015da9b8e522413352e58404754a7347b
Nymua <duboiscorenthin@gmail.com>
2017-02-06 17:18:11 +0100

    postgres

--- BackEnd/app/routes.js
+++ BackEnd/app/routes.js
@@ -1,6 +1,38 @@
+router.post('/', (req, res, next) => {
+  const results = [];
+  // Grab data from http request
+  const data = {text: req.body.text, complete: false};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    client.query();
+    // SQL Query > Select Data
+    const query = client.query('');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+})
 app.post('/signup', passport.authenticate('local-signup', {
     successRedirect : '/',
     failureRedirect : '/signup'
+}));
+
+app.post('/login', passport.authenticate('local-signup', {
+    successRedirect : '/',
+    failureRedirect : '/login'
 }));
 
 
