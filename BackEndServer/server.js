d9891143caffd309f64ccef73552a0a6a8aa832e
Nymua <duboiscorenthin@gmail.com>
2017-02-08 12:28:13 +0100

    routes

--- BackEndServer/server.js
+++ BackEndServer/server.js
@@ -6,12 +6,18 @@ var express = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     http = require('http'),
-    server = http.createServer(app)
+    server = http.createServer(app),
+    allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    }
 
 var routes = require('./routes/routes.js');
 
 var app = express();
-
+app.use(allowCrossDomain);
 app.set('superSecret', 'ilovepfe');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
